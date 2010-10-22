3e6f6370ca388db7fb2c503b355f58a2e901832b
Esteban Ordano <esteban@ordano.com.ar>
2010-10-22 17:29:18 -0300

    Added hashing

--- src/edu/itba/skbsolver/State.java
+++ src/edu/itba/skbsolver/State.java
@@ -10,10 +10,15 @@ import java.awt.Point;
  * @author eordano
  */
 public class State {
+
+	public static final int[] dx = {0,1,0,-1};
+	public static final int[] dy = {1,0,-1,0};
 	
 	public int[] boxes;
 	public int moves;
 	public int player;
+	public int hashCalculated = 0;
+	public State parent = null;
 	public Level map;
 	
 	/**
@@ -28,8 +33,56 @@ public class State {
 		this.player = player;
 		this.map = map;
 		this.moves = moves;
+		this.hashCalculated = player;
+		for (int i = 0; i < boxes.length; i++){
+			hashCalculated ^= (boxes[i] >>> i);
+		}
 	}
 	
+	/**
+	 * Create a State from a previous state and a box movement 
+	 * 
+	 * @param s
+	 * @param boxMoved
+	 * @param d
+	 */
+	public State(State s, int boxMoved, int direction, int playerMoves) {
+		// TODO Auto-generated constructor stub
+		this.parent = s;
+		this.boxes = s.boxes.clone();
+		this.moves = s.moves + playerMoves;
+		this.map = s.map;
+		this.player = s.boxes[boxMoved];
+		
+		this.boxes[boxMoved] += (dx[direction]<<16) + dy[direction];
+
+		// TODO: Reeview Zobrist key
+		this.hashCalculated = s.hashCalculated ^
+			s.player ^
+			(s.boxes[boxMoved] >>> boxMoved) ^
+			this.player ^
+			(this.boxes[boxMoved] >>> boxMoved);
+		
+		int newBox = boxMoved;
+		
+		while (newBox > 0 && this.boxes[newBox] < this.boxes[newBox-1]){
+			this.hashCalculated ^= (this.boxes[newBox] >>> newBox);
+			this.hashCalculated ^= (this.boxes[newBox-1] >>> newBox-1);
+			swap(this.boxes, newBox, newBox - 1);
+			this.hashCalculated ^= (this.boxes[newBox] >>> newBox);
+			this.hashCalculated ^= (this.boxes[newBox-1] >>> newBox-1);
+			newBox--;
+		}
+		while (newBox != boxes.length-1 && this.boxes[newBox] > this.boxes[newBox+1]){
+			this.hashCalculated ^= (this.boxes[newBox] >>> newBox);
+			this.hashCalculated ^= (this.boxes[newBox+1] >>> newBox+1);
+			swap(this.boxes, newBox, newBox + 1);
+			this.hashCalculated ^= (this.boxes[newBox] >>> newBox);
+			this.hashCalculated ^= (this.boxes[newBox+1] >>> newBox+1);
+			newBox++;
+		}
+	}
+
 	/**
 	 * @return player's X position
 	 */
@@ -50,6 +103,7 @@ public class State {
 	public int bx(int i){
 		return (boxes[i] >> 16) & 0xFFFF;
 	}
+	
 	/** 
 	 * @param i the index of the box
 	 * @return the y coordinate of the box
@@ -58,8 +112,33 @@ public class State {
 		return (boxes[i] & 0xFFFF);
 	}
 
+	/**
+	 * Retrieve the player's position as a Point
+	 * @return
+	 */
 	public Point playerPoint() {
 		return new Point(px(), py());
 	}
 
+	public boolean equals(State other){
+		if (this.player != other.player){
+			return false;
+		}
+		for(int i = 0; i < boxes.length; i++){
+			if (this.boxes[i] != other.boxes[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int hashCode(){
+		return this.hashCalculated;
+	}
+	
+	private void swap(int[] v, int a, int b){
+		int c = v[a];
+		v[a] = b;
+		v[b] = c;
+	}
 }
