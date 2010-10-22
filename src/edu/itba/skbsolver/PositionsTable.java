a2315da873054122730bd77f24512a837d86bb76
Esteban Ordano <esteban@ordano.com.ar>
2010-10-22 19:52:15 -0300

    Positions table implementada con un simple hashSet.

--- src/edu/itba/skbsolver/PositionsTable.java
+++ src/edu/itba/skbsolver/PositionsTable.java
@@ -1,9 +1,22 @@
 package edu.itba.skbsolver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PositionsTable {
+	
+	private Map<State, Boolean> map;
+	
+	public PositionsTable(){
+		map = new HashMap<State, Boolean>();
+	}
 
 	public boolean has(State newState) {
-		return false;
+		return map.containsKey(newState);
+	}
+	
+	public void add(State newState){
+		map.put(newState, true);
 	}
 	
 }
