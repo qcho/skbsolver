a307223741ec9e782e20eed9c308a4d2501880a6
Qcho <qcho86@gmail.com>
2010-10-23 12:58:57 -0300

    main

--- src/edu/itba/skbsolver/Main.java
+++ src/edu/itba/skbsolver/Main.java
@@ -1,8 +1,47 @@
 package edu.itba.skbsolver;
 
+import java.io.File;
+import java.security.InvalidParameterException;
+
+import com.sun.org.apache.xpath.internal.functions.WrongNumberArgsException;
+
 public class Main {
-	public static void main(String args[]){
-		// TODO: Launch a BFS and a DFS on each argument
-		// TODO: make a dot if that's what is requested
+	private static final String USAGE_HELP = "Usage: lefel-file method [tree] \n\n" + "  level-file   The file of the level.\n" + "      method   Either \"BFS\" or \"DFS.\n" + "        tree   Wheter to output .dot file or not.";
+
+	public static void main(String args[]) {
+
+		Solution sol = null;
+		boolean tree = false;
+		Level level = null;
+
+		try {
+			if (args.length < 2 || args.length > 3) {
+				throw new WrongNumberArgsException("Expected 2 or 3 parameters");
+			}
+
+			level = new Level(new File(args[0]));
+
+			if ("tree".equals(args[2])) {
+				tree = true;
+			}
+
+			if ("BFS".equals(args[1])) {
+				System.out.println("Running BFS");
+				sol = BFSRunner.run(level, tree);
+
+			} else if ("DFS".equals(args[1])) {
+				System.out.println("Running DFS");
+				sol = DFSRunner.run(level, tree);
+			} else {
+				throw new InvalidParameterException("Only BFS & DFS methods are provided.");
+			}
+
+			System.out.println("Best Solution: " + sol);
+
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			System.out.println(USAGE_HELP);
+		}
+
 	}
 }
